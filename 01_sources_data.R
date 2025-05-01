081efa73abb88a097ea4a94bb443db5c4d3a3d63
marcosqz <marcos.quijal@isglobal.org>
2025-05-01 12:07:06 +0200

    Changes: - Refinement of comments - model_param is now study_param - 2 code for plots removed (it will go to another file)

--- 01_sources_data.R
+++ 01_sources_data.R
@@ -2,7 +2,10 @@
 
 library(lubridate) # dow
 library(eurostat) # get_eurostat
-library(wcde) # get_wcde
+library(wcde) # get_wcde0
+# RClimChange is an experimetnal package
+# install.packages("devtools")
+# devtools::install_github("hllauca/RClimChange")
 library(RClimChange) # nex_download
 library(sf) # st_read / st_transform
 library(raster) # brick
@@ -10,33 +13,29 @@ library(exactextractr) # exact_extract
 
 #### SELECTED PARAMETERS ####
 
-model_param <- list(
-  age_groups = c("00_74", "75plus"), # Group 1 = 0 to 74 years old
-  # Group 2 = 75 and plus years old
-  ssp_scenario = 2,
+# LIST OF PARAMETERS FOR THE ILLUSTRATIVE HEALTH IMPACT PROJECTION STUDY
+study_param <- list(
+  age_groups = c("00_74", "75plus"), # <75 and ≥75 years
+  ssp_scenario = 2, # SSP2
   ssp_rcp_scenario = "ssp245", # c('ssp126, ssp245, ssp370, ssp585)
   selected_gcms = c("BCC-CSM2-MR", "MIROC6", "IPSL-CM6A-LR"), # DEFINE GCM MODELS
   selected_warming = "2")
 
 #### 1. SOURCES OF DATA ####
 
-### DATASET 1: TEMPERATURE AND MORTALITY OBSERVATIONS FOR LONDON ###
+### DATASET 1: TEMPERATURE AND MORTALITY OBSERVATIONS FOR LONDON ####
 data_tempmort <- read.csv("../../indata/lndn_obs.csv", 
                           colClasses = c(date = "Date"))
 
-range(data_tempmort$year) # Study period
-
-# RE-GROUP AGE CATEGORIES (00_74 AND 75_PLUS)
-# TODO: I THINK THIS SHOULD BE BETTER ALREADY BE DONE IN THE SAVED DATA
+# RE-GROUP AGE CATEGORIES (00_74 AND 75PLUS)
 data_tempmort$mort.00_74 <- data_tempmort$all_0_64 + data_tempmort$all_65_74
 data_tempmort$mort.75plus <- data_tempmort$all_75_84 + data_tempmort$all_85plus
 
-# Keep only specific columns and change the name of the object
-data_tempmort <- data_tempmort[, c("date", "year", "dow", "tmean", "mort.00_74", 
-                                   "mort.75plus")]
-data_tempmort$dow <- wday(data_tempmort$date)
+# SELECT RELEVANT COLUMNS
+data_tempmort <- data_tempmort[, c("date", "year", "dow", "tmean", 
+                                   "mort.00_74", "mort.75plus")]
 
-### DATASET 2: POPULATION OBSERVATIONS FOR LONDON ###
+### DATASET 2: POPULATION OBSERVATIONS FOR LONDON ####
 
 # DOWNLOAD POPULATION DATA FOR LONDON (UKI) FROM EUROSTAT
 data_popu <- get_eurostat("demo_r_d2jan", filters = list(geo = "UKI", 
@@ -49,17 +48,16 @@ data_popu$time <- NULL
 
 # SEPARATE POPULATION DATA BY AGE GROUPS
 # AGE GROUP UNDER 75 (less than 1yo and 1 to 74)
-data_popu_00_74 <- data_popu[
-  data_popu$age %in% c("Y_LT1", paste0("Y", 1:74)), ]
+data_popu_00_74 <- 
+  data_popu[data_popu$age %in% c("Y_LT1", paste0("Y", 1:74)), ]
 # AGE GROUP 75 PLUS(75 to 99 and open-ended age class)
-data_popu_75plus <- data_popu[
-  data_popu$age %in% c(paste0("Y", 75:99), "Y_OPEN"), ]
+data_popu_75plus <- 
+  data_popu[data_popu$age %in% c(paste0("Y", 75:99), "Y_OPEN"), ]
 
 # AGGREGATE POPULATION FOR THE TWO AGE GROUPS
 data_popu_00_74 <- setNames(
   aggregate(values ~ year, data = data_popu_00_74, FUN = sum),
   c("year", "popu.00_74"))
-
 data_popu_75plus <- setNames(
   aggregate(values ~ year, data = data_popu_75plus, FUN = sum),
   c("year", "popu.75plus"))
@@ -68,12 +66,12 @@ data_popu_75plus <- setNames(
 data_popu <- merge(data_popu_00_74, data_popu_75plus)
 rm(data_popu_00_74, data_popu_75plus)
 
-### DATASET 3: POPULATION AND MORTALITY PROJECTIONS FOR UK AND NI ###
+### DATASET 3: POPULATION AND MORTALITY PROJECTIONS FOR UK AND NI ####
 
 # DOWNLOAD POPULATION PROJECTIONS (1 value every 5 year: 1950, 1955, 1960, ...)
 proj_popu <- get_wcde(
   indicator = "pop", # Population Size (000's)
-  scenario = model_param$ssp_scenario,
+  scenario = study_param$ssp_scenario,
   pop_age =  "all",
   pop_sex = "both",
   version = "wcde-v2", # we use v2, because v3 only has population from 2000
@@ -83,7 +81,7 @@ colnames(proj_popu)[colnames(proj_popu) == "pop"] <- "popu"
 # DOWNLOAD SURVIVAL RATIO PROJECTIONS (1 value every 5 year-period: 1950-1955, 1955-1960, ...)
 proj_mort <- get_wcde(
   indicator = "assr", # Age-Specific Survival Ratio
-  scenario = model_param$ssp_scenario,
+  scenario = study_param$ssp_scenario,
   pop_age =  "all",
   version = "wcde-v2", # we use v2, because v3 only has population from 2000
   country_name = "United Kingdom of Great Britain and Northern Ireland")
@@ -119,7 +117,7 @@ proj_mortpopu$age <- NULL
 proj_mortpopu <- aggregate(cbind(popu, mort) ~ age_group + year, 
                            data = proj_mortpopu, FUN = sum)
 
-# PROJECTIONS FRON LONG TO WIDE
+# RESPHAPE THE PROJECTION DATASET FROM LONG TO WIDE
 proj_mortpopu <- reshape(proj_mortpopu, timevar = "age_group", idvar = "year", 
                          direction = "wide")
 
@@ -127,55 +125,14 @@ proj_mortpopu <- reshape(proj_mortpopu, timevar = "age_group", idvar = "year",
 proj_mortpopu$mort.00_74 <- proj_mortpopu$mort.00_74/5
 proj_mortpopu$mort.75plus <- proj_mortpopu$mort.75plus/5
 
-# CALCULATE TOTAL MORTALITY AND POPULATION
-proj_mortpopu$mort <- proj_mortpopu$mort.00_74 + proj_mortpopu$mort.75plus
-proj_mortpopu$popu <- proj_mortpopu$popu.00_74 + proj_mortpopu$popu.75plus
-
-# PLOT PROJECTIONS OF MORTALITY
-par(mfrow = c(1, 2), oma = c(0, 0, 3, 0))
-plot(x = proj_mortpopu$year, 
-     y = proj_mortpopu$mort.00_74, type = "l",
-     ylim = c(0, max(proj_mortpopu$mort.00_74, proj_mortpopu$mort.75plus)),
-     main = "Mortality projections", 
-     xlab = "Year", ylab = "Number of deahts", 
-     col = "#1B9E77", lwd = 2)
-lines(x = proj_mortpopu$year, 
-      y = proj_mortpopu$mort.75plus, 
-      lwd = 2, col = "#D95F02")
-abline(h = 0)
-legend("bottomleft", legend = c("<74", "+75"), fill = c("#1B9E77", "#D95F02"), 
-       border = "black", bty = "o", cex = 0.8)
-
-# PLOT PROJECTIONS OF POPULATION PERCERTANGE
-plot(x = proj_mortpopu$year, 
-     y = rep(0.5, length(proj_mortpopu$year)), 
-     type = "n", ylim = c(0, 100),
-     main = "Populations projections", 
-     xlab = "Year", ylab = "Percentage (%)")
-polygon(x = c(proj_mortpopu$year, rev(proj_mortpopu$year)), 
-        y = c(rep(0, length(proj_mortpopu$year)), 
-              rev(proj_mortpopu$popu.00_74/proj_mortpopu$popu*100)), 
-        col = "#1B9E77", border = NA)
-polygon(c(proj_mortpopu$year, rev(proj_mortpopu$year)),
-        c(100 - proj_mortpopu$popu.75plus/proj_mortpopu$popu*100, 
-          rev(rep(100, length(proj_mortpopu$popu.75plus)))), 
-        col = "#D95F02", border = NA)
-legend("bottomright", legend = c("<74"), fill = c("#1B9E77"), 
-       border = "black", bty = "o", cex = 0.8)
-legend("topright", legend = c("+75"), fill = c("#D95F02"), 
-       border = "black", bty = "o", cex = 0.8)
-
-mtext(paste0("SSP", model_param$ssp_scenario, " - UK and Northern Ireland"), outer = TRUE, 
-             cex = 1.5)
-
-### DATASET 4: TEMPERATURE PROJECTIONS FOR COMBINATIONS OF SSP/RCP ###
+### DATASET 4: TEMPERATURE PROJECTIONS FOR COMBINATIONS OF SSP/RCP ####
 
 # DOWNLOAD TEMPERATURE PROJECTIONS FOR SSP2-RCP4.5 IN THE FUTURE PERIOD (2015-2100)
 run_download <- FALSE # Run the downloading process only once
 if(run_download == TRUE) {
   nex_download(location = getwd(),
-               model = model_param$selected_gcms,
-               scenario = model_param$ssp_rcp_scenario,
+               model = study_param$selected_gcms,
+               scenario = study_param$ssp_rcp_scenario,
                variable = 'tas',
                years = 2015:2100,
                version = NULL,
@@ -186,7 +143,7 @@ if(run_download == TRUE) {
 # DOWNLOAD TEMPERATURE PROJECTIONS FOR THE HISTORICAL PERIOD (1950-2014)
 if(run_download == TRUE) {
   nex_download(location = getwd(),
-               model = model_param$selected_gcms,
+               model = study_param$selected_gcms,
                scenario = 'historical',
                variable = 'tas',
                years = 1950:2014,
@@ -207,13 +164,13 @@ shp_london <- st_transform(shp_london, 4326)
 var_path <- c("BCC-CSM2-MR" = "gn", "MIROC6" = "gn", "IPSL-CM6A-LR" = "gr")
 
 # LOOP GCMS AND YEARS TO EXTRACT THE CELLS OF THEMPERATURES WITHIN THE BOUNDARIES
-proj_temp <- lapply(model_param$selected_gcms, function(i_gcm){ # LOOP SELECTED GCMS
+proj_temp <- lapply(study_param$selected_gcms, function(i_gcm){ # LOOP SELECTED GCMS
   
   proj_temp <- lapply(1950:2100, function(i_year) { # LOOP YEARS
     
     print(paste0(i_gcm, ": ", i_year))
     
-    # Load Raster Data
+    # LOAD RASTER DATA
     if(i_year < 2015) {
       file_path <- paste0("../../tas/historical/", i_gcm, 
                           "/tas_day_", i_gcm, 
@@ -254,15 +211,15 @@ proj_temp <- lapply(model_param$selected_gcms, function(i_gcm){ # LOOP SELECTED 
   
 })
 proj_temp <- do.call(rbind, proj_temp)
+rm(shp_london)
 
 # PROJECTIONS FRON LONG TO WIDE
 proj_temp <- reshape(proj_temp, timevar = "gcm", idvar = "date", 
                      direction = "wide")
 proj_temp <- proj_temp[order(proj_temp$date),]
 
-# SOME GCMs MISS THE 31ST DECEMBER OF LEAP YEARS 
+# SOME GCMs (BCC-CSM2-MR) MISS THE 31ST DECEMBER OF LEAP YEARS 
 # IMPUTE THE MEAN VALUE FROM THE PRECEDING AND FOLLOWING DAYS.
-
 na_dates <- proj_temp[is.na(proj_temp$`temp.BCC-CSM2-MR`),]$date
 
 for(i_date in na_dates){
@@ -270,38 +227,12 @@ for(i_date in na_dates){
   i_date <- as.Date(i_date)
   
   proj_temp[proj_temp$date == i_date, "temp.BCC-CSM2-MR"] <- 
-    mean(proj_temp[proj_temp$date %in% c(i_date-1, i_date+1), "temp.BCC-CSM2-MR"])
+    mean(
+      proj_temp[proj_temp$date %in% c(i_date-1, i_date+1), "temp.BCC-CSM2-MR"])
 }
 rm(i_date, na_dates)
 
-### illustrative plot
-
-# # PLOT THE RASTER AND THE SHAPEFILE 
-# file_path <- paste0("../../tas/ssp245/", model_param$selected_gcms[1], 
-#                     "/tas_day_", model_param$selected_gcms[1], 
-#                     "_ssp245_r1i1p1f1_", var_path[model_param$selected_gcms[1]],
-#                     "_", 2100, ".nc")
-# raster_data <- raster::brick(file_path)
-# 
-# # Some NetCDF files have longitudes ranging from 0 to 360 instead of -180 to 180.
-# # This adjustment ensures compatibility with the London shapefile
-# if (xmin(raster_data) > 180) {
-#   extent(raster_data) <- extent(xmin(raster_data) - 360,
-#                                 xmax(raster_data) - 360,
-#                                 ymin(raster_data),
-#                                 ymax(raster_data))
-# }
-# 
-# # pdf(file = "../../outdata/plot/fig1_resolution_ssp_data.pdf", 
-# #     width = 5, height = 5)
-# plot(raster_data[[226]], xlab = "Longitude", ylab = "Latitude",
-#      main = "Mean temperature 2100-08-15 \n(SSP2-RCP4.5, KIOST-ESM)")
-# plot(shp_london$geometry, add = TRUE)
-# # dev.off()
-
-rm(shp_london)
-
-### DATASET 5: WARMING LEVELS ###
+### DATASET 5: WARMING LEVELS ####
 
 # READ THE CSV FILE DIRECTLY INTO R
 data_warming <- read.csv(
@@ -309,24 +240,24 @@ data_warming <- read.csv(
   stringsAsFactors = FALSE)
 
 # KEEP ONLY THE SELECTED GCMS
-data_warming <- lapply(model_param$selected_gcms, function(x) {
+data_warming <- lapply(study_param$selected_gcms, function(x) {
   data_warming[grepl(x, data_warming$model_run),]
 })
 data_warming <- do.call(rbind, data_warming)
 
 # KEEP ONLY THE SELECTED SSP/RCP SCENARIO
 data_warming <- data_warming[, 
-   grepl(model_param$ssp_rcp_scenario, colnames(data_warming)) | # columns that include the name of the ssp/rcp scenario
-   colnames(data_warming) == "model_run"] # keep also the column with the gcm models
+                             grepl(study_param$ssp_rcp_scenario, colnames(data_warming)) | # columns that include the name of the ssp/rcp scenario
+                               colnames(data_warming) == "model_run"] # keep also the column with the gcm models
 data_warming$model_run <- sub("_.*", "", data_warming$model_run) # keep only the name of the scenario (e.g. BCC-CSM2-MR_r1i1p1f1 to BCC-CSM2-MR)
 
 # DATASET FROM WIDE TO LONG
 data_warming <- reshape(data_warming,
-                          varying = which(names(data_warming) != "model_run"),
-                          v.names = "year", 
-                          timevar = "warming_level", 
-                          times = names(data_warming)[names(data_warming) != "model_run"],
-                          direction = "long")
+                        varying = which(names(data_warming) != "model_run"),
+                        v.names = "year", 
+                        timevar = "warming_level", 
+                        times = names(data_warming)[names(data_warming) != "model_run"],
+                        direction = "long")
 
 # CLEAN THE NAMES OF THE DATASET
 data_warming$warming_level <- 
@@ -381,7 +312,7 @@ data_warming$year2 <- data_warming$year + 10
 
 #### SAVE DATASETS ####
 
-save(model_param, file = "../../outdata/file/mode_parameters.RData")
+save(study_param, file = "../../outdata/file/mode_parameters.RData")
 save(data_tempmort, file = "../../outdata/file/data_tempmort.RData")
 save(data_popu, file = "../../outdata/file/data_popu.RData")
 save(proj_mortpopu, file = "../../outdata/file/data_projection_mortality_population_ssp2.RData")
