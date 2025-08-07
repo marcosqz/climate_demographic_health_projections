063fd13abae73f7af451f1a5924f78f7d2e26ffb
marcosqz <marcos.quijal@isglobal.org>
2025-08-07 14:09:10 +0200

    Remove creation of fixed temperature projections:
    - Changes in the functions, since alternatives to consider fixed temperature projections are not needed anymore.
    
    - For the general bias-correction, now I bias-correct everything (1950-2099) in one single call, instead of doing it by decadal sub calibration periods.

--- 04_climate_change_scenarios.R
+++ 04_climate_change_scenarios.R
@@ -1,13 +1,6 @@
 ################################################################################
 
-# TODO: Check in the text below if in the alternative projection series we are
-# really matching with the 2007-2011 observed or the 2007-2011 projected bias-corrected series
-
 # TODO: Investigate and ask about the source of the isimip3 function (change name of the file)
-
-# TODO: Check if in the first calibration is it necessary to split up the long
-# period into small calibration periods, do we have the same results if I put only
-# one long calibration period with all the period?
 
 # This script aligns projected and observed temperatures, calibrating GCM 
 # outputs using the ISIMIP3BASD method, with observed temperatures at the 
@@ -29,146 +22,76 @@ source("isimip3_from_masselot.R")
 #### BIAS-CORRECTION TEMPERATURE PROJECTIONS ###################################
 
 # KEEP ONLY TEMPEARTURE OBSERVATIONS FROM THE MORT/TEMP DATASET
-data_temp <- data_tempmort[, c("date", "tmean")]
+data_temp <- subset(data_tempmort, select = c("date", "tmean"))
 rm(data_tempmort)
 
-# BUILD FUNCTION TO APPLY BIAS CORRECTION TO TEMPERATURE PROJECTIONS
-Run_BC_Temperature_Projections <- function(
-    data_obshist,     # Observed historical temperature data
-    data_simhist,     # Historical modeled temperature from GCMs
-    data_simfut,      # Future modeled temperature from GCMs
-    period_obshist = NULL,  # Period to use from observed historical data
-    period_simhist = NULL,  # Period to use from historical simulations
-    period_simfut = NULL,   # Period to use from future projections
-    var_obshist,      # Var name or if 'use_gcm' takes paste0("temp.", i_gcm)
-    var_simhist,      # Var name or if 'use_gcm' takes paste0("temp.", i_gcm)
-    var_simfut,       # Var name or if 'use_gcm' takes paste0("temp.", i_gcm)
-    calperiod         # Loop over some calibration periods
-    ){
+# DEFINE PERIODS
+period_obshist <- 1990:2011 # Observational historical period (reference)
+period_simhist <- 1990:2011 # Simulations historical period (used to calculate the bias to respect to the observations)
+period_simfut <- 1950:2099 # Simulations future period (where the bc is applied)
+
+# LOOP THROUGH EACH SELECTED GCM
+proj_temp_bc <- lapply(study_param$selected_gcm, function(i_gcm) {
   
-  # LOOP THROUGH EACH SELECTED GCM
-  proj_temp_bc <- lapply(study_param$selected_gcm, function(i_gcm) {
+  print(paste0("Run bias-correction: GCM ",  
+               i_gcm, " (", which(i_gcm == study_param$selected_gcm), "/",
+               length(study_param$selected_gcm), ")"))
+  
+  # LOOP OVER EACH CALENDAR MONTH
+  proj_temp_bc <- lapply(1:12, function(i_month){
     
-    print(paste0("Run bias-correction: GCM ",  
-                 i_gcm, " (", which(i_gcm == study_param$selected_gcm), "/",
-                 length(study_param$selected_gcm), ")"))
+    # SUBSET DATASETS FOR GIVEN MONTH AND YEAR PERIOD
+    subset_obshist <- subset(
+      data_temp, 
+      (year(date) %in% period_obshist) & (month(date) == i_month))
+    subset_simhist <- subset(
+      proj_temp, 
+      (year(date) %in% period_simhist) & (month(date) == i_month))
+    subset_simfut <- subset(
+      proj_temp, 
+      (year(date) %in% period_simfut) & (month(date) == i_month))
     
-    # GENERATE VARIABLES NAMES IF "use_gcm" is specified
-    if (var_obshist == "use_gcm") {var_obshist <- paste0("temp.", i_gcm)}
-    if (var_simhist == "use_gcm") {var_simhist <- paste0("temp.", i_gcm)}
-    if (var_simfut == "use_gcm") {var_simfut <- paste0("temp.", i_gcm)}
+    # EXTRACT TEMPERATURE VALUES
+    obshist <- subset_obshist$tmean
+    simhist <- subset_simhist[[paste0("temp.", i_gcm)]]
+    simfut <- subset_simfut[[paste0("temp.", i_gcm)]]
     
-    # LOOP THROUGH EACH CALIBRATION PERIOD
-    proj_temp_bc <- lapply(calperiod, function(i_calperiod) {
-      
-      # DEFAUL TO CURRENT CALIBRATION PERIOD IF NONE PROVIDED
-      if (is.null(period_obshist)) {period_obshist <- i_calperiod}
-      if (is.null(period_simhist)) {period_simhist <- i_calperiod}
-      if (is.null(period_simfut)) {period_simfut <- i_calperiod}
-      
-      # LOOP OVER EACH CALENDAR MONTH
-      proj_temp_bc <- lapply(1:12, function(i_month){
-        
-        # SUBSET DATASETS FOR GIVEN MONTH AND YEAR PERIOD
-        subset_obshist <- subset(
-          data_obshist, 
-          (year(date) %in% period_obshist) & (month(date) == i_month))
-        subset_simhist <- subset(
-          data_simhist, 
-          (year(date) %in% period_simhist) & (month(date) == i_month))
-        subset_simfut <- subset(
-          data_simfut, 
-          (year(date) %in% period_simfut) & (month(date) == i_month))
-        
-        # EXTRACT TEMPERATURE VALUES
-        obshist <- subset_obshist[[var_obshist]]
-        simhist <- subset_simhist[[var_simhist]]
-        simfut <- subset_simfut[[var_simfut]]
-        
-        # EXTRACT YEARS FOR ALL THREE DATASETS
-        yearobshist <- year(subset_obshist[["date"]])
-        yearsimhist <- year(subset_simhist[["date"]])
-        yearsimfut <- year(subset_simfut[["date"]])
-        
-        # APPLY ISIMIP3 BIAS CORRECTION METHOD
-        temp_bc <- isimip3(
-          obshist = obshist,
-          simhist = simhist,
-          simfut = simfut,
-          yearobshist = yearobshist,
-          yearsimhist = yearsimhist,
-          yearsimfut = yearsimfut)
-        
-        proj_temp_bc <- data.frame(
-          date = subset_simfut[["date"]],
-          temp = temp_bc[,1])
-        
-        return(proj_temp_bc)
-        
-      })
-      
-      # COMBINE ALL MONTHS FOR THIS CALIBRATION PERIOD
-      proj_temp_bc <- do.call(rbind, proj_temp_bc)
-      
-      return(proj_temp_bc)
-      
-    })
+    # EXTRACT YEARS FOR ALL THREE DATASETS
+    yearobshist <- year(subset_obshist$date)
+    yearsimhist <- year(subset_simhist$date)
+    yearsimfut <- year(subset_simfut$date)
     
-    # COMBINE ALL CALIBRATION PERIODS FOR THIS GCM
-    proj_temp_bc <- do.call(rbind, proj_temp_bc)
-    colnames(proj_temp_bc)[colnames(proj_temp_bc) == "temp"] <- 
-      paste0("temp.", i_gcm)
+    # APPLY ISIMIP3 BIAS CORRECTION METHOD
+    temp_bc <- isimip3(
+      obshist = obshist,
+      simhist = simhist,
+      simfut = simfut,
+      yearobshist = yearobshist,
+      yearsimhist = yearsimhist,
+      yearsimfut = yearsimfut)
+    
+    proj_temp_bc <- data.frame(
+      date = subset_simfut$date,
+      temp = temp_bc[,1])
     
     return(proj_temp_bc)
     
   })
-  # MERGE THE DATASETS FOR ALL GCMs IN A WIDE FORMAT
-  proj_temp_bc <- 
-    Reduce(function(x, y) merge(x, y, by = "date", all = TRUE), proj_temp_bc)
+  
+  # COMBINE ALL MONTHS
+  proj_temp_bc <- do.call(rbind, proj_temp_bc)
+  colnames(proj_temp_bc)[colnames(proj_temp_bc) == "temp"] <- 
+    paste0("temp.", i_gcm)
   
   return(proj_temp_bc)
   
-}
-
-# APPLY BIAS CORRECTION TO FUTURE PROJECTIONS
-proj_temp_bc <- Run_BC_Temperature_Projections(
-  data_obshist = data_temp,
-  data_simhist = proj_temp,
-  data_simfut = proj_temp,
-  period_obshist = 1990:2011,
-  period_simhist = 1990:2011,
-  period_simfut = NULL, # NULL to take calibration period from calperiod
-  var_obshist = "tmean",
-  var_simhist = "use_gcm",
-  var_simfut = "use_gcm",
-  calperiod = list(
-    1950:2011,
-    2012:2029,
-    2030:2039,
-    2040:2049, 
-    2050:2059,
-    2060:2069, 
-    2070:2079,
-    2080:2089, 
-    2090:2099))
-
-# APPLY BIAS CORRECTION TO HELD TEMPERATURE CONSTANT OVER THE PROJECTION PERIOD
-proj_temp_constant <- Run_BC_Temperature_Projections(
-  data_obshist = proj_temp_bc,
-  data_simhist = proj_temp_bc,
-  data_simfut = proj_temp_bc,
-  period_obshist = 2007:2011,
-  period_simhist = NULL, # NULL to take calibration period from calperiod
-  period_simfut = NULL, # NULL to take calibration period from calperiod
-  var_obshist = "use_gcm",
-  var_simhist = "use_gcm",
-  var_simfut = "use_gcm",
-  calperiod = lapply(0:17, function(i){2010:2014+5*i}))
+})
+# MERGE THE DATASETS FOR ALL GCMs IN A WIDE FORMAT
+proj_temp_bc <- 
+  Reduce(function(x, y) merge(x, y, by = "date", all = TRUE), proj_temp_bc)
 
 #### SAVE OUTPUTS ##############################################################
 save(proj_temp_bc, file = paste0(
   "outdata/file/03_calibrated_climate_projections/data_proj_temp_biascorrection_",
   study_param$ssp_rcp_scenario,".RData"))
-save(proj_temp_constant, file = paste0(
-  "outdata/file/03_calibrated_climate_projections/data_proj_temp_constant_",
-  study_param$ssp_rcp_scenario,".RData"))
+
